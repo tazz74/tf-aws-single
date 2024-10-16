5c54719a9c4ba410883c2b652ab910c617f53b36
Erik Scholten <tazz74@gmail.com>
2024-10-16 11:20:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.71.0"
+      version = "5.72.0"
     }
   }
 }
 
 provider "aws" {
   region  = "eu-central-1"
-}
-
 
 resource "aws_instance" "Web-server" {
     ami = "ami-0084a47cc718c111a"
@@ -29,4 +27,4 @@ resource "aws_instance" "Web-server" {
     chkconfig httpd on
     EOF
 }
-
+}
