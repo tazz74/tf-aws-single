2db873f96aea664c30c408c1180dca29c5eca57e
Erik Scholten <tazz74@gmail.com>
2024-10-16 12:23:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "Web-server" {
     instance_type = "t2.micro"
 
     subnet_id = aws_subnet.public.id
-    vpc_security_group_ids = [aws_security_group.default.id]
+    vpc_security_group_ids = [aws_security_group.web_sg.id]
     associate_public_ip_address = true
 
     user_data = <<-EOF
