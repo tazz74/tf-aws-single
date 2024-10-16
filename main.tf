bfef491aa377f08cdc0660d1e32235d9a75e8ef4
Erik Scholten <tazz74@gmail.com>
2024-10-16 13:07:31 +0200

    .

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "Web-server" {
   instance_type = "t2.micro"
   
   subnet_id = var.subnet_id
-  vpc_security_group_ids = var.security_group_id
+  vpc_security_group_ids = [var.security_group_id]
   associate_public_ip_address = true
 
   user_data = <<-EOF
